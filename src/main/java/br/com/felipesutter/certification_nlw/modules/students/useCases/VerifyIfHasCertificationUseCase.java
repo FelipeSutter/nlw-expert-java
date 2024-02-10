8910b538351443dc8608e016e25c6918b941c8d9
FelipeSutter <lipeps61@gmail.com>
2024-02-10 13:23:17 -0300

    add new relationships between classes with spring.jpa

--- src/main/java/br/com/felipesutter/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/br/com/felipesutter/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,14 +1,20 @@
 package br.com.felipesutter.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.felipesutter.certification_nlw.modules.students.dto.VerifyHasVerificationDTO;
+import br.com.felipesutter.certification_nlw.modules.students.repository.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    CertificationStudentRepository repository;
+
     public boolean execute(VerifyHasVerificationDTO dto) {
-        if (dto.getEmail().equals("felipesutter@gmail.com") && dto.getTechnology().equals("Java")) {
+        var result = this.repository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         }
         return false;
