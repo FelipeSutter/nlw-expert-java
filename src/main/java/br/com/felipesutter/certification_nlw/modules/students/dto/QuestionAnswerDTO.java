577f228a6db1e29bce1d77c676ba7f2665c4d33f
FelipeSutter <lipeps61@gmail.com>
2024-02-14 16:30:46 -0300

    Criado DTO's para question and student além do repository de student

---
+++ src/main/java/br/com/felipesutter/certification_nlw/modules/students/dto/QuestionAnswerDTO.java
@@ -0,0 +1,18 @@
+package br.com.felipesutter.certification_nlw.modules.students.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class QuestionAnswerDTO {
+
+    private String questionID;
+    private String alternativeID;
+    private boolean isCorrect;
+
+}
